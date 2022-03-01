53cc79068c17d893719e4861ff3bc33de38007d0
Azhman <bapir.dev@gmail.com>
2022-03-01 19:03:27 +0330

    Initial Commit

---
+++ CoinRadar/CoinRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinRadarApp.swift
+//  CoinRadar
+//
+//  Created by Amirreza Zarepour on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CoinRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinRadar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoinRadar
+//
+//  Created by Amirreza Zarepour on 3/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
